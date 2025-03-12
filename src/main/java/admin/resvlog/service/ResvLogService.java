72d63d8e2660c513c7957f753366aefb436e0e98
ithodol <ddmdjesz1@gmail.com>
2025-03-12 19:45:13 +0900

    feat : 3/12 j 전은서 예매 로그 전체 조회 백 일부 작성

---
+++ src/main/java/admin/resvlog/service/ResvLogService.java
@@ -0,0 +1,20 @@
+/*
+package admin.resvlog.service;
+
+import admin.resvlog.model.mapper.ResvLogMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResvLogService {
+    @Autowired
+    private ResvLogMapper resvLogMapper;
+
+    // 예매내역 전체 조회
+    public List<ResvLog> findAll(){
+        return resvLogMapper.findAll();
+    }
+}
+*/
