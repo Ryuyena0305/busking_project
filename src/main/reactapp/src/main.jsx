d6973f0dbd1902ebd0e7c5a60ce0f55645743146
2ãmxnxu <leeeun099@naver.com>
2025-03-12 20:35:02 +0900

    feat : 3/12 f 이민수 사용자 화면단 구현

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './reservation/App.jsx';  
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.querySelector('#root'));  
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>  
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/main/reactapp/src/reservation/Res.jsx
@@ -0,0 +1,8 @@
+export default function Res() {
+    return (
+        <div>
+            <h1>자동예매 페이지</h1>
+            <p>여기서 자동 예매를 진행할 수 있습니다.</p>
+        </div>
+    );
+}
