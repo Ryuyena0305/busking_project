bcded9d239f398445f6fec55f36b49bcb1472a7d
2ãmxnxu <leeeun099@naver.com>
2025-03-13 18:04:21 +0900

    feat 3/13 이민수 b 사용자 예매 가능 날짜, 도착지, 시간, 좌석 조회

--- src/main/java/user/controller/ResController.java
+++ src/main/java/user/controller/ResController.java
@@ -1,4 +1,38 @@
 package user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import user.service.ResService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resv")
 public class ResController {
+
+    @Autowired
+    private ResService resService;
+
+    @GetMapping("/date")
+    public List<String> getDates() {
+        return resService.getStart();
+    }
+
+    @GetMapping("/dest")
+    public List<String> getLocation(@RequestParam("startdate") String startdate) {
+        return resService.getLocation(startdate);
+    }
+
+    @GetMapping("/time")
+    public List<String> getStartTime(@RequestParam("startdate") String startdate, @RequestParam("dest") String dest) {
+        return resService.getStartTime(startdate, dest);
+    }
+    @GetMapping("/seat")
+    public List<String> getSeat(@RequestParam("startdate") String startdate, @RequestParam("dest") String dest, @RequestParam("starttime") String starttime) {
+        return resService.getSeat(startdate, dest, starttime);
+    }
+
 }
--- src/main/java/user/model/mapper/ResMapper.java
+++ src/main/java/user/model/mapper/ResMapper.java
@@ -1,4 +1,26 @@
 package user.model.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
 public interface ResMapper {
+    @Select("SELECT DISTINCT startdate FROM timetable WHERE startdate")
+    List<String> getStart();
+
+    @Select("SELECT DISTINCT l.dest FROM timetable t JOIN location l ON t.locid = l.locid JOIN resv r ON t.timeid = r.timeid WHERE t.startdate = #{startdate}")
+    List<String> getLocation(@Param("startdate") String startdate);
+
+    @Select("SELECT DISTINCT t.starttime FROM timetable t JOIN location l ON t.locid = l.locid WHERE t.startdate = #{startdate} AND l.dest = #{dest}")
+    List<String> getStartTime(@Param("startdate") String startdate, @Param("dest") String dest);
+
+    @Select("SELECT DISTINCT b.bsnum FROM busseat b JOIN timetable t ON b.btid = t.biid JOIN location l ON t.locid = l.locid WHERE t.startdate = #{startdate} AND l.dest = #{dest} AND t.starttime = #{starttime}")
+    List<String> getSeat(@Param("startdate") String startdate, @Param("dest") String dest, @Param("starttime") String starttime);
+
+
+
 }
