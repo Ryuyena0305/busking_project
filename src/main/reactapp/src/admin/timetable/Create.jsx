0da99693495a88e6d059e43af7d0dc2efc958936
ithodol <ddmdjesz1@gmail.com>
2025-03-14 18:11:20 +0900

    feat : 3/14 전은서 f 스케줄 등록 구현 중

--- src/main/reactapp/src/admin/timetable/Create.jsx
+++ src/main/reactapp/src/admin/timetable/Create.jsx
@@ -1,3 +1,11 @@
+import axios from 'axios';
+
 export default function Create(props){
+    return(<>
+        <div id='wrap'>
+            <div>스케줄 등록</div>
+            
+        </div>
+    </>)
     
 }
