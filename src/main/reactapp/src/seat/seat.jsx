f46327da61ee5aca86f067831732c8df2cdeb59d
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-03-14 15:22:04 +0900

    3/14 f 류예나 좌표통한 좌석배치 구현중

--- src/main/reactapp/src/seat/seat.jsx
+++ src/main/reactapp/src/seat/seat.jsx
@@ -7,39 +7,59 @@ import Button from '@mui/joy/Button';
 
 export default function seat(props) {
     const [seats, setSeats] = useState([]);
+    const [binum, setBinum] = useState();
 
-   
-    
+
+
     const navigate = useNavigate();
     const { biid } = useParams();
 
     useEffect(() => {
         onGet();
-    }, [biid]); 
+    }, [biid]);
 
     const onGet = async () => {
         const response = await axios.get(`http://localhost:8080/busseat?biid=1`);
         console.log(response.data);
+        console.log(response.data.binum);
+
         setSeats(response.data);
+        setBinum(response.data[0].binum);
     }
 
     const onPut = async (bsnum, bsstate) => {
-        const response = await axios.put(`http://localhost:8080/busseat?biid=1&bsnum=${bsnum}`, {biid:1,bsnum:bsnum,bsstate:bsstate});
-       if(response.data==true){onGet();}
-     
+        const response = await axios.put(`http://localhost:8080/busseat?biid=1&bsnum=${bsnum}`, { biid: 1, bsnum: bsnum, bsstate: bsstate });
+        if (response.data == true) { onGet(); }
+
     }
+
     
+
 
     return (
         <>
-            <div>
+            <div className="buswrap">
                 <h5>좌석 출력</h5>
+
+
                 <div className="bus">
+                    <div className="buswidth">
+                        <img className="driverImg" src="../etc/driver.png"></img>
+                        <div>{binum}<br/>&nbsp;&nbsp;&nbsp;&nbsp;BUS </div>
+
+                        <img className="driverImg" src="../etc/ent.png"></img>
+                    </div>
+                    <div className="btncontain">
                     {
+                        
                         seats.map((seat, index) => {
+                            
+                            console.log(seats[index].x)
+                            console.log(seats[index].y)
+                            
                             return (
-                                <div key={index}>
-                                   
+
+                                <div className="seatbtn" key={index}>
                                     {seat.bsstate == 1 ? (
                                         <Button className="statebtn" onClick={() => { onPut(seat.bsnum, 0) }} variant="outlined">{seat.bsnum}</Button>
                                     ) : (
@@ -47,8 +67,14 @@ export default function seat(props) {
                                     )}
                                 </div>
                             )
-                        })
+
+                        }
+
+
+                        )
+                        
                     }
+                    </div>
                 </div>
             </div>
         </>
