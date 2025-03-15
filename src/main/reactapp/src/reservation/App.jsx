b03fa5908439c50708907e13ae9d27a45e8a6a78
ithodol <ddmdjesz1@gmail.com>
2025-03-16 06:03:15 +0900

    feat : 3/15 f 전은서 사용자 홈화면 페이지 이동 추가

--- src/main/reactapp/src/reservation/App.jsx
+++ src/main/reactapp/src/reservation/App.jsx
@@ -1,7 +1,12 @@
 // App.jsx
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapPin } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { Route, Routes, Link, useLocation } from 'react-router-dom';
 import Res from './Res.jsx';
 import AutoRes from './AutoRes.jsx';
+import admin from '../admin/App.jsx';
+
 import './App.css';
 
 
@@ -16,13 +21,12 @@ export default function App(props) {
     const day = getDate.getDate();
     const hours = getDate.getHours();
     const minutes = getDate.getMinutes();
-    const seconds = getDate.getSeconds();
 
     const today = `${year}년 ${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
     const time = `${hours < 10 ? '0' + hours : hours}시 ${minutes < 10 ? '0' + minutes : minutes}분`;
 
 
-
+    
     return (
         <div className="container">
             <div className="content">
@@ -31,7 +35,15 @@ export default function App(props) {
                         <div className='tit'>[ 버스 승차권 발매기 ]</div>
                         <div className='subTit'>
                             <div className='left'>
-                                <div>&#128681; 인천</div>
+                                <Link to = "/">
+                                    <FontAwesomeIcon icon={faMapPin} className="admin"/>
+                                </Link> {/* 링크 연결 필요 */}
+                                <div>&nbsp;&nbsp;인천</div>
+                            </div>
+                            <div>
+                                <Link to = "/">
+                                    <FontAwesomeIcon icon={faHouse} className="home"/>
+                                </Link> {/* 링크 연결 필요 */}
                             </div>
                             <div className='right'>
                                 <div className='date'>{today}</div>
