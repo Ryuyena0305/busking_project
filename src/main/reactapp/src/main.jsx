9c7dcc3cf7b1e788044bee841567de221c06d165
2mxnxu <leeeun099@naver.com>
2025-03-16 20:38:29 +0900

    fix 3/16 f 이민수 예약가능 날짜 선택

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './reservation/App.jsx';
+import App from './reservation/App.jsx';
 import Seat from './seat/seat.jsx';
 import ResvSeat from './seat/resvseat.jsx';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,8 +10,7 @@ const root = createRoot(document.querySelector('#root'));
 root.render(
   <StrictMode>
     <BrowserRouter>
-      {/*<App />*/}<Seat/>
-      <ResvSeat />
+      {<App/>}
     </BrowserRouter>
   </StrictMode>
 );
