67e76e67a5595fa937ea6fd7e634f65af83ae440
2ãmxnxu <leeeun099@naver.com>
2025-03-17 18:25:50 +0900

    feat : 3/17 f 이민수 목적시 선택페이지로 이동하게 경로 설정, 날짜 선택 후 누르는 버튼 생성

---
+++ src/main/reactapp/src/reservation/Dest.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export default function Dest(props) {
+    return (
+        <>
+            <h2>목적지 선택</h2>
+        </>
+    );
+}
---
+++ src/main/reactapp/src/reservation/Main.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+export default function Main(){
+
+    const getDate = new Date();
+    const year = getDate.getFullYear();
+    const month = getDate.getMonth() + 1;
+    const day = getDate.getDate();
+    const hours = getDate.getHours();
+    const minutes = getDate.getMinutes();
+
+    const today = `${year}년 ${month < 10 ? '0' + month : month}월 ${day < 10 ? '0' + day : day}일`;
+    const time = `${hours < 10 ? '0' + hours : hours}시 ${minutes < 10 ? '0' + minutes : minutes}분`;
+    
+    return(<>             
+    <div className="content">
+        {location.pathname !== '/StartDate' && location.pathname !== '/AutoRes' && (
+            <div className='realcontent'>
+                <div className='mainbtn'>
+                    <Link to="/StartDate">
+                        <button className='nomal'>일반예매</button>
+                    </Link>
+                    <Link to="/AutoRes">
+                        <button className='auto'>자동예매</button>
+                    </Link>
+                </div>
+                <div className='lang'>
+                    <button>한국어</button>
+                    <button>English</button>
+                    <button>中國語</button>
+                    <button>日本語</button>
+                </div>
+            </div>
+        )}
+    </div></>)
+}
