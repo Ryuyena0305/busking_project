bc9aa34bf4828b06eef588f69f9e28af1ac22688
ithodol <ddmdjesz1@gmail.com>
2025-03-17 19:06:44 +0900

    chore : 3/17 f 상세조회 링크 수정

--- src/main/reactapp/src/admin/timetable/TviewBus.jsx
+++ src/main/reactapp/src/admin/timetable/TviewBus.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import axios from 'axios';
 import './timetable.css';
 import Table from '@mui/joy/Table';
+import { Link } from 'react-router-dom';
 
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
@@ -25,49 +26,49 @@ function MyTable(props){
         <table>
             <thead>
                 <tr>
-                    <th>번호</th><th>출발일자</th><th>출발시간</th><th>차량정보</th><th>운전기사</th><th>비고</th>
+                    <th>번호</th><th>출발일자</th><th>출발시간</th><th>차량정보</th><th>운전기사</th><th>도착지</th><th>비고</th>
                 </tr>
             </thead>
             <tbody> 
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
                 <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td>
-                    <td><button type='button'>상세조회</button></td>
+                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
+                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
             </tbody>
         </table>
