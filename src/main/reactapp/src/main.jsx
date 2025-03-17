9070958787bc09322323f48f0bd3c2d2c4844401
ithodol <ddmdjesz1@gmail.com>
2025-03-17 18:22:13 +0900

    chore : 3/17 f 전은서 오류 주석

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -1,19 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './reservation/App.jsx';
-import Seat from './seat/seat.jsx';
-import ResvSeat from './seat/resvseat.jsx';
-import ResvSeat2 from './seat/resvseat2.jsx';
+//import App from './reservation/App.jsx';
+//import Seat from './seat/seat.jsx';
+// import ResvSeat from './seat/resvseat.jsx';
+// import ResvSeat2 from './seat/resvseat2.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.querySelector('#root'));  
 
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </StrictMode>
-);
-// import App from './admin/App.jsx';
-// root.render(<App />);
+// root.render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App/>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
+import App from './admin/App.jsx';
+root.render(<App />);
