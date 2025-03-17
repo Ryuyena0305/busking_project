ecb75605184d68f651dd3e6a51045d3fe2a7399a
ithodol <ddmdjesz1@gmail.com>
2025-03-17 21:43:41 +0900

    chore : 3/17 f 전은서 항목별 조회 샘플 제거

--- src/main/reactapp/src/admin/timetable/TviewBus.jsx
+++ src/main/reactapp/src/admin/timetable/TviewBus.jsx
@@ -1,24 +1,11 @@
 import * as React from 'react';
 import axios from 'axios';
 import './timetable.css';
-import Table from '@mui/joy/Table';
 import { Link } from 'react-router-dom';
 
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-{/*function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-*/}
 
 
 function MyTable(props){
@@ -34,42 +21,8 @@ function MyTable(props){
                     <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
                     <td><Link className='link'><button type='button'>상세조회</button></Link></td>
                 </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
-                <tr className='bodyTr'>
-                    <td>1</td><td>2020.02.20</td><td>06:00</td><td>39서2934</td><td>전은서</td><td>부산</td>
-                    <td><Link className='link'><button type='button'>상세조회</button></Link></td>
-                </tr>
+
+                
             </tbody>
         </table>
     </>)
@@ -88,6 +41,10 @@ export { Page };
 
 
 export default function TviewBus(props){
+
+
+
+
 
     return(<>
             <div id="container"> 
@@ -113,28 +70,3 @@ export default function TviewBus(props){
     </>)
 
 }
-
-{/*
-                    <Table hoverRow>
-                        <thead>
-                            <tr>
-                                <th>번호</th>
-                                <th>출발일자</th>
-                                <th>출발시간</th>
-                                <th>운전기사</th>
-                                <th>비고</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {rows.map((row) => (
-                            <tr key={row.name}>
-                                <td>{row.name}</td>
-                                <td>{row.calories}</td>
-                                <td>{row.fat}</td>
-                                <td>{row.carbs}</td>
-                                <td>{row.protein}</td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-*/}
