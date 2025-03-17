5a5f8d7298065031de5f8d5270c880ebe8d76200
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-03-17 18:22:22 +0900

    feat : 3/17 f 류예나 좌석배치수정

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './reservation/App.jsx';
+// import App from './reservation/App.jsx';
 import Seat from './seat/seat.jsx';
 import ResvSeat from './seat/resvseat.jsx';
 import ResvSeat2 from './seat/resvseat2.jsx';
@@ -11,7 +11,10 @@ const root = createRoot(document.querySelector('#root'));
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App/>
+      {/* <App/> */}
+      <Seat/>
+      <ResvSeat/>
+      <ResvSeat2/>
     </BrowserRouter>
   </StrictMode>
 );
