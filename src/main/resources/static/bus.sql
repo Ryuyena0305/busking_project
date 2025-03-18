1e61493269c9452d6c6ff900147c113753738827
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-03-18 17:32:55 +0900

    merge : 3/18 4명 전부 중간병합 및 DB수정

--- src/main/resources/static/bus.sql
+++ src/main/resources/static/bus.sql
@@ -88,10 +88,7 @@ insert into location (dest,locprice) values('강릉','36000');
 insert into location (dest,locprice) values('대구','41000');
 insert into location (dest,locprice) values('대전','18000');
 insert into location (dest,locprice) values('전주','24100');
-insert into location (dest,locprice) values('수원','6100');
-insert into location (dest,locprice) values('당진','5000');
-insert into location (dest,locprice) values('포항','37800');
-insert into location (dest,locprice) values('용인','6200');
+
 
 select * from location;
 
