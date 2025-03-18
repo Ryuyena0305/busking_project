cc8c3fc7ec854287ec506a5a20bc12c1f7a4e238
ithodol <ddmdjesz1@gmail.com>
2025-03-18 23:12:08 +0900

    chore : 3/18 f 전은서 주석 삭제

--- src/main/reactapp/src/admin/timetable/Timetable.jsx
+++ src/main/reactapp/src/admin/timetable/Timetable.jsx
@@ -5,23 +5,6 @@ import './timetable.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-// import axios from 'axios';
-
-
-
-// // select에서 사용할 차량정보 가져오기
-// export default async function getBus( 매개변수 , 매개변수2 )  {
-//     try{
-//         //const response = await axios.get(`http://localhost:8080/timetable/getbus`)
-//         const response = await axios.get( 매개변수2 )
-//         console.log(response.data);
-//         //setSelectBuss(response.data);
-//         매개변수(response.data);
-//     }catch(error) {
-//         console.log(error);
-//     }
-// } // getData end
-
 // select에서 사용할 차량정보 가져오기
 export default function GetBusData(props){
     const [biid, setBiid] = useState('');
