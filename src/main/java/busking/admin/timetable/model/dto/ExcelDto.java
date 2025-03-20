dbf2f25ed15880d7f5cad2453d54a70d4aabce1d
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-03-20 20:43:51 +0900

    feat : 3/20 f 류예나 엑셀파일 다운 코드작성 중

---
+++ src/main/java/busking/admin/timetable/model/dto/ExcelDto.java
@@ -0,0 +1,24 @@
+package busking.admin.timetable.model.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExcelDto {
+
+
+        private int timeid;
+        private String starttime;
+        private String startdate;
+        private int biid;
+        private int locid;
+        private String binum;
+        private String driver;
+        private String dest;
+
+
+}
