a5d1f2364f0a44863f0e8245d80d213212eed1cf
ithodol <ddmdjesz1@gmail.com>
2025-03-21 13:02:17 +0900

    feat : 3/21 b 전은서 페이지네이션 라이브러리 적용

--- src/main/java/busking/admin/timetable/service/TimeTableService.java
+++ src/main/java/busking/admin/timetable/service/TimeTableService.java
@@ -2,15 +2,20 @@ package busking.admin.timetable.service;
 
 import busking.admin.timetable.model.dto.TimeTableDto;
 import busking.admin.timetable.model.mapper.TimeTableMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TimeTableService {
-    @Autowired
-    private TimeTableMapper timeTableMapper;
+    private final TimeTableMapper timeTableMapper;
+
+
 
     // 버스 정보 가져오기
     public List<TimeTableDto> getBusInfo(){
@@ -43,18 +48,24 @@ public class TimeTableService {
     }
 
     // (도착지별)스케줄 조회
-    public List<TimeTableDto> locView(int locid){
-        return timeTableMapper.locView(locid);
+    public PageInfo<TimeTableDto> locView(int locid, int pageNum, int pageSize){
+        PageHelper.startPage(pageNum, pageSize); // 페이징 적용
+        List<TimeTableDto> schedules = timeTableMapper.locView(locid); // MyBatis 호출
+        return new PageInfo<>(schedules); // 페이징 정보를 포함한 PageInfo 반환
     }
 
     // (버스별)스케줄 조회
-    public List<TimeTableDto> busView(int biid){
-        return timeTableMapper.busView(biid);
+    public PageInfo<TimeTableDto> busView(int biid, int pageNum, int pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<TimeTableDto> schedules = timeTableMapper.busView(biid);
+        return new PageInfo<>(schedules);
     }
 
     // (일자별)스케줄 조회
-    public List<TimeTableDto> dateView(String startdate){
-        return timeTableMapper.dateView(startdate);
+    public PageInfo<TimeTableDto> dateView(String startdate, int pageNum, int pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<TimeTableDto> schedules = timeTableMapper.dateView(startdate);
+        return new PageInfo<>(schedules);
     }
 
 }
