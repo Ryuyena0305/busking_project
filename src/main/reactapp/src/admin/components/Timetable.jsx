600b6bab4f1295903c0de47f1c1fe39021a0b639
ithodol <ddmdjesz1@gmail.com>
2025-03-21 19:36:21 +0900

    feat : 3/21 f 전은서 스케줄 수정 수정

--- src/main/reactapp/src/admin/components/Timetable.jsx
+++ src/main/reactapp/src/admin/components/Timetable.jsx
@@ -10,15 +10,23 @@ import { useEffect } from 'react';
 
 
 // select에서 사용할 차량정보 가져오기
+// props로 한번에 받음(Tview.jsx 부모-> TimeTable.jsx 자식)
+    // findBiid={paramBiid} className='getBusData' value={times.biid} defaultBiid={ times.biid }
+    //  name="biid"
+    //  times={times}
+    //  setTimes={setTimes}
+// 부모에 있는 변수들을 자식에서 쓸 수 있음
 export default function GetBusData( props ){
     const [biid, setBiid] = useState('');
     const [selectBuss, setSelectBuss] = useState([]);
 
-    const findBiid = props.findBiid;
-    console.log( props );
+    //console.log(selectBuss);
+    
+    console.log( props )
 
-    let 차량초기번호 = "차량초기번호" in props ? props.차량초기번호 : '' ;
-    console.log( 차량초기번호 );
+    
+    let defaultBiid = "defaultBiid" in props ? props.defaultBiid : '' ;
+    console.log( defaultBiid );
 
     const getBus = async () => {
         try{
@@ -37,17 +45,21 @@ export default function GetBusData( props ){
     // biid가 변경될 때마다 biid를 GetBusData에 전달
     useEffect(() => {
        if (biid) {
-            findBiid(biid);
+        props.findBiid(biid);
         }
-    }, [biid, findBiid]);
+    }, [biid, props.findBiid]);
 
-    const onReset = () => {
-        
+
+    const 수정함수 = (e) =>{
+        setBiid(e.target.value)
+        "setTimes" in props && props.setTimes( { ...props.times , "biid"  : e.target.value } )
+        // setTimes가 props에 존재하면 뒤 코드 실행해라
     }
+    
 
     return(<>
         <div className='viewFind'>차량 정보</div>
-        <select className='subCont' value={ biid == '' ? 차량초기번호 : biid  } onChange={(e) => setBiid(e.target.value)}>
+        <select className='subCont' value={ biid == '' ? defaultBiid : biid  } onChange={ 수정함수  }>
             <option value="0">선택</option>
         {
             selectBuss.map((selectBus, index) => {
@@ -62,15 +74,22 @@ export default function GetBusData( props ){
 
 
 // select에서 사용할 터미널 정보 가져오기
-export function GetLocData({findLocid}){
+export function GetLocData(props){
     const [locid, setLocid] = useState('');
     const [selectLocs, setSelectLocs] = useState([]);
+
+    const findLocid = props.findLocid;
+    console.log( props );
+    
+    let defaultLocid = "defaultLocid" in props ? props.defaultLocid : '' ;
+    console.log( defaultLocid );
 
     const getLoc = async () => {
         try{
             const response = await axios.get(`http://localhost:8080/timetable/getloc`)
             //console.log(response.data);
             setSelectLocs(response.data);
+            console.log(setSelectLocs);
         }catch(error) {
             console.log(error);
         }
@@ -80,17 +99,24 @@ export function GetLocData({findLocid}){
         getLoc();
     }, [])
 
-    // Locid가 변경될 때마다 biid를 GetBusData에 전달
+    // Locid가 변경될 때마다 locid를 GetBusData에 전달
     useEffect(() => {
         if (locid) {
                 findLocid(locid);
             }
         }, [locid, findLocid]);
 
+    const 수정함수2 = (e) =>{
+        setLocid(e.target.value)
+        "setTimes" in props && props.setTimes( { ...props.times , "locid"  : e.target.value } )
+    }
+
+
+
     return(<>
         <div className='viewFind'>터미널 정보</div>
-            <select className='subCont' value={locid} onChange={(e) => setLocid(e.target.value)}>
-                <option value="">선택</option>
+            <select className='subCont' value={ locid == '' ? defaultLocid : locid  } onChange={수정함수2}>
+                <option value="0">선택</option>
             {
                 selectLocs.map((selectLoc, index) => {
                     return(
