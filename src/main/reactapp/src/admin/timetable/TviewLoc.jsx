425bada758cbba47fe975af6b0d291b2d7a55028
ithodol <ddmdjesz1@gmail.com>
2025-03-21 13:02:57 +0900

    feat : 3/21 b 전은서 페이지네이션 라이브러리 적용

--- src/main/reactapp/src/admin/timetable/TviewLoc.jsx
+++ src/main/reactapp/src/admin/timetable/TviewLoc.jsx
@@ -68,7 +68,7 @@ export default function TviewLoc(){
                             }
                         </tbody>
                     </table>
-                    <Page/>
+                    
         
 
                 </div>
