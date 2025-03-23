13fea650602ae48698c79bd22e7478bdc9e303a7
ithodol <ddmdjesz1@gmail.com>
2025-03-24 00:55:55 +0900

    feat : 3/24 b 전은서 사용하지 않는 import 삭제

--- src/main/reactapp/src/admin/timetable/TviewDate.jsx
+++ src/main/reactapp/src/admin/timetable/TviewDate.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import axios from 'axios';
 import './timetable.css';
-import {MyTable, Page} from './TviewBus.jsx'
+
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
