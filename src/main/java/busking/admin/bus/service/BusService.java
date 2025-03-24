28b4cd171272b80993ce68a76f3bfbabe846e454
ithodol <ddmdjesz1@gmail.com>
2025-03-24 19:15:46 +0900

    feat : 3/24 b 전은서 예매로그 페이지네이션 적용 완료

--- src/main/java/busking/admin/bus/service/BusService.java
+++ src/main/java/busking/admin/bus/service/BusService.java
@@ -2,6 +2,9 @@ package busking.admin.bus.service;
 
 import busking.admin.bus.model.BusDto;
 import busking.admin.bus.model.BusMapper;
+import busking.admin.resvlog.model.dto.ResvlogDto;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +18,10 @@ public class BusService {
     public boolean onCreate(BusDto busDto){
         return busMapper.onCreate(busDto);
     }
-    public List<BusDto> onRead(){
-        return busMapper.onRead();
+    public PageInfo<BusDto> onRead(int pageNum, int pageSize){
+        PageHelper.startPage(pageNum, pageSize); // 페이징 적용
+        List<BusDto> schedules = busMapper.onRead(); // MyBatis 호출
+        return new PageInfo<>(schedules); // 페이징 정보를 포함한 PageInfo 반환
     }
     public BusDto onView(int biid){
         return busMapper.onView(biid);
