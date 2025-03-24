9583f800c2aab439a5f1295d32a9f2254c887ffd
ithodol <ddmdjesz1@gmail.com>
2025-03-24 19:16:11 +0900

    feat : 3/24 f 전은서 예매로그 페이지네이션 적용 완료

--- src/main/reactapp/src/admin/Read.jsx
+++ src/main/reactapp/src/admin/Read.jsx
@@ -2,15 +2,42 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './timetable/timetable.css';
+
+import PaginationComponent from './components/Pagination';
+
+
 export default function Read(props) {
+  const [buses, setBuses] = useState({list: [], total: 0, pages: 1});
+
+  const [page, setPage] = useState(1); // 페이지 기본값
+  const pageSize = 10; // 한페이지에 표시할 개수
+
+
+  const onRead = async () => {
+    try{
+      const response = await axios.get(`http://localhost:8080/bus/businfo?page=${page}&pageSize=${pageSize}`);
+
+      const {list, total} = response.data
+      console.log(response.data);
+      const totalPages = Math.ceil(total / pageSize) // 전체 페이지수 구하기
+      setBuses({list, total, pages : totalPages});
+    }catch(error){
+      console.log(error);
+    }
+    
+  };
+
   useEffect(() => {
     onRead();
-  }, []);
-  const onRead = async () => {
-    const response = await axios.get('http://localhost:8080/bus/businfo');
-    setBuses(response.data);
-  };
-  const [buses, setBuses] = useState([]);
+  }, [page]);
+
+  // 페이지 변경 시 호출되는 함수
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);  // 새로운 페이지 번호로 업데이트
+    //console.log("New page selected:", newPage);
+  }
+  //console.log("getViewLists.pages", getViewLists.pages);
+  
   return (
     <>
       <div id="container">
@@ -28,9 +55,9 @@ export default function Read(props) {
             </tr>
           </thead>
           <tbody>
-            {buses.map((bus, index) => {
+            {buses.list && buses.list.map((bus, index) => {
               return (
-                <tr className='bodyTr'>
+                <tr className='bodyTr' key={index}>
                   <td>{bus.biid}</td>
                   <td>{bus.driver}</td>
                   <td>{bus.binum}</td>
@@ -50,6 +77,9 @@ export default function Read(props) {
             })}
           </tbody>
         </table>
+        <PaginationComponent
+            count={buses.pages || 1} page={page} onChange={handlePageChange}
+        />
         
         </div>
       </div>
